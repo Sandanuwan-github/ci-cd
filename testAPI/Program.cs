249eb06115fc7770be92845963204168494e2905
Sandanuwan-github <sandanuwan.dh@gmail.com>
2025-10-30 18:14:34 +0530

    added world endpoint

--- testAPI/Program.cs
+++ testAPI/Program.cs
@@ -36,6 +36,9 @@ app.MapGet("/weatherforecast", () =>
  // New simple endpoint
  app.MapGet("/hello", () => "Hello from your .NET API! This is a new endpoint.");
 
+ // New simple endpoint
+ app.MapGet("/world", () => "Hello World from your .NET API! This is a new endpoint.");
+ 
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
